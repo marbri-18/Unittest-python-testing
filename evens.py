c6c03ebcd9a0d4760c90ec09d71cf66404e9f598
Martin Bridgman <martin.bridgman@gmail.com>
2022-03-06 23:20:42 +0000

    unittest set up

---
+++ evens.py
@@ -0,0 +1,14 @@
+def even_number_of_evens(numbers):
+    """
+    Should raise a typeError if a list is not passed into the function
+    error message: "A list was not passed into the function"
+    if numbers is empty return False
+    if the number of even numbers is odd, return False
+    if the number of even numbers is 0, return False
+    if the number of even numbers is even, return True
+    """
+    return True
+
+
+if __name__ == "__main__":
+    print(even_number_of_evens(5))
